7761de8360ad5375422bfe39280d168800b78b7d
xuqing <208547233@qq.com>
2020-06-24 11:23:13 +0800

    第一次向GitHub添加代码

---
+++ demo01.py
@@ -0,0 +1,51 @@
+"""
+print("hello world!")  #  字符串
+print(2333)  #  整数
+print(2.333)  #  小数
+print(True)  #  布尔值  True  False
+print(())  #  元组
+print([])  #  数组
+print({})  #  字典
+
+锄禾日当午
+汗滴禾下土
+
+print("哈哈",233,2.333)
+print("哈哈"+"嘻嘻")  #  字符串的拼接
+print("哈哈"*100)
+print(1+2)
+print(((1+2)*100-9.9)/2)
+print(2>3)
+#  变量
+#  赋值
+name = "张三"  #  把张三这个值赋值各科名字叫name的变量
+print(name)
+
+a = input("请输入：")
+b = input("请输入：")
+print("input获取的内容: ",a+b)
+#  数据格式的转换
+
+
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# a = float (input("请输入："))
+# b = float (input("请输入："))
+# print("input获取的内容: ",a+b)
+
+
+# a = 'dfsdfsdggfhgjghdsfdfhfgjhkhjrdtgdrgdfgdfgrter'
+# print(len(a))
+"""
+
+a = input("请输入第一段内容:")
+b = input("请输入第二段内容:")
+print("长度等于",len(a+b))
+
